0d7e960fc6dc04a4d9ea5c90e0f132d1e50ca6d6
Jerson Paniagua <diazpany@gmail.com>
2025-05-18 23:19:10 -0300

    add example 4 responds_to

---
+++ 12-equality-classes/ex_4_docment_identifier_responds_to.rb
@@ -0,0 +1,17 @@
+class DocumentIdentifier
+  attr_reader :folder, :name
+
+  def initialize( folder, name )
+    @folder = folder
+    @name = name
+  end
+
+  def ==(other)
+    return true if other.equal?(self)
+    return false unless other.instance_of?(self.class)
+    folder == other.folder && name == other.name
+  end
+end
+
+class ContractIdentifier < DocumentIdentifier
+end 
